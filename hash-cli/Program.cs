279715b7cf16430ade0a583ceda5fe96bf1028ed
jus1d <artfa63@gmail.com>
2022-12-03 00:43:18 +0400

    initial commit

---
+++ hash-cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hash_cli 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("started");
+        }
+    }
+}
