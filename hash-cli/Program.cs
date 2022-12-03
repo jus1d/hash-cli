72757a9b06f8bf18af6b9f950262b0e9e2191ae3
jus1d <artfa63@gmail.com>
2022-12-03 15:10:41 +0400

    add md5 hash, LogHash

--- hash-cli/Program.cs
+++ hash-cli/Program.cs
@@ -2,7 +2,7 @@
 using System.Security.Cryptography;
 using System.Text;
 
-namespace hash_cli 
+namespace hash_cli
 {
     class Program
     {
@@ -12,33 +12,65 @@ namespace hash_cli
             {
                 if (args[0][0] == '-')
                 {
-                    string parameter = args[0];
+                    string parameter = args[0].ToLower();
                     if (parameter == "--help" || parameter == "-h")
                     {
-                        Console.WriteLine("help command");
+                        Console.WriteLine("\nUsage: hash-cli [ hash-algorithm ] [ raw-data ]\n" +
+                                          "\n  or\n" +
+                                          "\nUsage for hashing files: hash-cli [ hash-algorithm ] --file [ file-name ]\n" +
+                                          "\nFile-name parameter may be clear => will opens a window for selecting a file\n");
                     }
                 }
                 else
                 {
-                    string hashType = args[0];
+                    string hashType = args[0].ToLower();
+                    string rawData = args[1];
+                    
 
                     switch (hashType)
                     {
                         case "sha256":
-                            string rawData = args[1];
 
-                            string hash = Sha256(rawData);
+                            if (rawData == "--file" || rawData == "-f")
+                            {
+                                Console.WriteLine("Hashing files will available soon");
+                            }
+                            else
+                            {
+                                string hash = Sha256(rawData);
                             
-                            Console.WriteLine($"hash:sha256 -> {hash}");
+                                LogHash(rawData, hashType, hash);
+                            }
                             
+                            break;
+                        
+                        case "md5":
+
+                            if (rawData == "--file" || rawData == "-f")
+                            {
+                                Console.WriteLine("Hashing files will available soon");
+                            }
+                            else
+                            {
+                                {
+                                    string hash = Md5(rawData);
+                                    
+                                    LogHash(rawData, hashType, hash);
+                                }
+                            }
                             break;
                     }
                 }
             }
             catch (Exception e)
             {
-                Console.WriteLine("Type --help or -h, to see command list");
+                Console.WriteLine("Use --help or -h flags, to see usage list");
             }
+        }
+
+        static void LogHash(string rawData, string hashType, string hash)
+        {
+            Console.WriteLine($"{rawData}:{hashType} -> {hash}");
         }
 
         static string Sha256(string rawData)
@@ -55,6 +87,17 @@ namespace hash_cli
                 }
 
                 return builder.ToString();
+            }
+        }
+
+        static string Md5(string rawData)
+        {
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(rawData);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                return Convert.ToHexString(hashBytes).ToLower();
             }
         }
     }
